701d9317f0062ecbb018ef4dfc8188015b72b248
Sweepyoface <github@sweepy.pw>
2017-02-07 00:18:21 -0500

    Taking a different direction with this

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 **Description:**
 
-This will "fade" (make them translucent and nocollided) when you pick up props to stop minginess and assist with building. The effect will automatically go away when it's frozen. It also sets the velocity to 0 when it is let go of to disable throwing props, and there is an option to completely disable prop damage. It comes with a fully featured config.
+This will "fade" (make them translucent and nocollided) when you pick up props to stop minginess and assist with building. The effect will automatically go away when it's frozen. It also sets the velocity to 0 when it is let go of to disable throwing props. It comes with a fully featured config.
 
 Requests and issues are appreciated!
---
+++ lua/autorun/server/sv_propsystem.lua
@@ -0,0 +1,97 @@
+PropMingeConfig = {} -- DO NOT EDIT
+
+------- BEGIN CONFIG -------
+
+PropMingeConfig.IgnoreGroups = { -- Groups listed here will be exempt from the effects
+	"group1",
+	"group2"
+}
+
+PropMingeConfig.IgnoreSteamIDs = { -- Same as IgnoreGroups, but for SteamIDs
+	"STEAM_0:0:11",
+	"STEAM_0:0:00"
+}
+
+PropMingeConfig.NoCollideEntities = { -- The entities you want the effect to be applied to
+	"prop_physics",
+	"gmod_cameraprop",
+	"Keypad",
+	"gmod_button"
+}
+
+PropMingeConfig.EnableAutoNocollide = true -- Enable the automatic nocollide when physgunning a prop?
+PropMingeConfig.EnableAutoNocollideConstraints = true -- Enable the automatic nocollide for all constrained props while physgunning a prop?
+PropMingeConfig.EnableTransparency = true -- Slightly fade props when they're being nocollided?
+PropMingeConfig.AlphaFade = 200 -- The alpha the prop is set to while being physgunned (max 255)
+
+------- END OF CONFIG -------
+
+function IsPlayerInside(ent) -- Check if there's a player or vehicle inside ent
+	local check = false
+	local center = ent:LocalToWorld(ent:OBBCenter())
+	for _,v in next, ents.FindInSphere(center, ent:BoundingRadius() - 20) do
+		if v:IsPlayer() or v:IsVehicle() then
+			return true
+		end
+	end
+end
+
+function OnPickup(ply, ent) -- On physgun pickup, nocollide the prop and make it slightly transparent
+	if (PropMingeConfig.EnableAutoNocollide) then
+		if (ent:CPPICanPhysgun(ply)) then
+			if (!table.HasValue(PropMingeConfig.IgnoreSteamIDs, ply:SteamID())) and (!table.HasValue(PropMingeConfig.IgnoreGroups, ply:GetUserGroup())) then
+				if (table.HasValue(PropMingeConfig.NoCollideEntities, ent:GetClass())) then
+					ent:SetCollisionGroup(COLLISION_GROUP_WORLD) -- Nocollide
+					local col = ent:GetColor()
+					if (PropMingeConfig.EnableTransparency) then
+						ent:SetColor(Color(col.r, col.g, col.b, (PropMingeConfig.AlphaFade)))
+						ent:SetRenderMode(RENDERMODE_TRANSALPHA) -- Set transparency
+					end
+					if (PropMingeConfig.EnableAutoNocollideConstraints) then -- Do the same for all connected props
+						for _, ent in pairs(constraint.GetAllConstrainedEntities(ent)) do
+							ent:SetCollisionGroup(COLLISION_GROUP_WORLD)
+							local col = ent:GetColor()
+							if (PropMingeConfig.EnableTransparency) then
+								ent:SetColor( Color( col.r, col.g, col.b, (PropMingeConfig.AlphaFade) ) )
+								ent:SetRenderMode(RENDERMODE_TRANSALPHA)
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+end
+hook.Add("PhysgunPickup", "On Pickup", OnPickup)
+
+function ResetVelocity(ply, ent) -- On physgun drop, set velocity to 0
+	ent:SetVelocity(Vector(0, 0, 0))
+end
+hook.Add("PhysgunDrop", "Reset Velocity", ResetVelocity)
+
+function OnFreeze(weapon, phys, ent, ply)
+	if (PropMingeConfig.EnableAutoNocollide) then
+		if (table.HasValue(PropMingeConfig.NoCollideEntities, ent:GetClass())) then
+			if (ent:CPPICanPhysgun(ply)) then -- Make sure they're actually allowed to do it
+				if not (IsPlayerInside(ent)) then -- Make sure it's not obstructed by a player
+					if (IsValid(ent)) then
+						ent:SetCollisionGroup(COLLISION_GROUP_NONE) -- Return prop to norma;
+						ent:SetRenderMode(RENDERMODE_NORMAL)
+						local col = ent:GetColor()
+						ent:SetColor( Color( col.r, col.g, col.b, 255) )
+						if (PropMingeConfig.EnableAutoNocollideConstraints) then -- Do the same for all connected props, if enabled
+							for _, ent in pairs( constraint.GetAllConstrainedEntities( ent ) ) do
+								ent:SetCollisionGroup(COLLISION_GROUP_NONE)
+								ent:SetRenderMode(RENDERMODE_NORMAL)
+								local col = ent:GetColor()
+								ent:SetColor( Color( col.r, col.g, col.b, 255) )
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+end
+hook.Add("OnPhysgunFreeze", "On Freeze", OnFreeze)
+
